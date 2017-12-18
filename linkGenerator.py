995ef830c20f8476f278ac26b2e086fd71e09ac8
yolossn <nssvlr@gmail.com>
2017-12-19 01:56:58 +0530

    use phantomJS,handle 404

--- linkGenerator.py
+++ linkGenerator.py
@@ -1,33 +1,39 @@
-from contextlib import closing
 from selenium.webdriver import PhantomJS
-from selenium.webdriver import Firefox
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-
+import requests
 def getHtmlSource(url,time=10):
-    with closing(Firefox()) as browser:
-        browser.get(url)
-        WebDriverWait(browser,timeout=time)
-        source=browser.page_source
+    driver=PhantomJS( service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any', '--web-security=false'])
+    driver.get(url)
+    WebDriverWait(driver,timeout=time)
+    source=driver.page_source
+    #driver.save_screenshot('a.png')
     return source
+
 def getLinks(url):
     source=getHtmlSource(url)
     soup=BeautifulSoup(source,'html.parser')
     divs= soup.findAll("div", { "class" : "l-u-Ab-T-j" })
     names=soup.findAll("span",{ "class" : "l-Ab-T-r" })
+    def ids(x):
+        x=x.get("id")
+        x=x.lstrip(":g.")
+        x=x.rstrip(":label")
+        return x
+    names=[span.text for span in names]
+    divs=[ids(div) for div in divs]
+    links={}
     #print(names)
-    lnames=[]
-    links=[]
-    for name in names:
-        lnames.append(name.text)
-    for div in divs:
-        ids=div.get("id")
-        ids=ids.lstrip(":g.")
-        ids=ids.rstrip(":label")
-        links.append(ids)
-    for n,l in zip(names,ids):
-        print("video={}\nDownload_link=https://drive.google.com/uc?export=download&id={}\n".format(n,l))
+    #print(divs)
+    for name,div in zip(names,divs):
+        url="https://drive.google.com/uc?export=download&id={}".format(div)
+        resp=requests.head(url)
+        print(url)
+        if resp.status_code!=404:
+            links[name]=url
+            print("video={}\nDownload_link={}\n".format(name,url))
+    return links
 if __name__=="__main__":
-    url="" #enter the url
-    getLinks(url)
+   url="" #enter the url
+   links=getLinks(url)
 
